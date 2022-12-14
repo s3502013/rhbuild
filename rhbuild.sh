f6d061cb95fc6b3b76d3e2018c6a3cf84402c2da
s3502013 <45354906+s3502013@users.noreply.github.com>
2022-12-15 07:57:43 +1100

    Create rhbuild.sh
    Scripty boi

---
+++ rhbuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Base Image Update"
+
+# Subscription Management Repository Update – This will enable all file types
+
+sudo subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms -y
+
+sleep 2
+
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+
+echo "Step One completed..."
+sleep 2
+
+sudo yum install ntfs-3g -y
+
+# Virtual Machine Manager install
+
+echo "Virtual Machine Manager Hypervisor Installation"
+
+sudo yum install virt-manager -y
+
+echo "Remember to update packages using:"
+echo "sudo dnf update"
+echo "or:"
+echo "sudo yum update"
